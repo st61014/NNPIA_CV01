ba1dccf7e70fbe01e08f048dbaef194857b50e91
st61014 <st61014@upce.cz>
2023-02-15 17:59:29 +0100

    XML, JSON, YAML differences comment

--- src/main/java/cz/upce/fei/NNPIA_CV01/controller/HelloController.java
+++ src/main/java/cz/upce/fei/NNPIA_CV01/controller/HelloController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.*;
 
 //@Controller se používá pro označení třídy jako Spring MVC Controller.
 //@RestController anotace je speciální kontroler pro RESTful Web služby, je to kombinace anotací @Controller a @ResponseBody
+
+//XML se podobá HTML, ale používá jiné značky. XML je zkratka pro eXtensible Markup Language, což znamená, že můžete vytvářet vlastní značky.
+//JSON je zkratka pro JavaScript Object Notation. JSON je textový a čitelný pro člověka. Používá {} a indentace.
+//YAML je zkratka pro YAML Ain't Markup Language. YAML je textový a čitelný pro člověka. Používá odsazení, pomlčky a dvojtečky.
 @RestController
 public class HelloController {
     @GetMapping("")
